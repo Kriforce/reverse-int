e4dc8dfba50e539f82a7ddd98d98fc5fdb027b42
Kristaps <kriforce7@gmail.com>
2023-01-10 13:28:37 +0300

    feat: add solution of task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n) { 
+    let str = n + '';
+    if (str[0] == '-') return str.slice(1).split('').reverse().join('');
+    else return str.split('').reverse().join('');
 }
